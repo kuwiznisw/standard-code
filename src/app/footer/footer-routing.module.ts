ccda6065a2ae0e5bd892c1a33df7c42bf281f9f3
Akmal <akmal@senwave.com>
2022-04-27 15:05:44 +0800

    nav

--- src/app/footer/footer-routing.module.ts
+++ src/app/footer/footer-routing.module.ts
@@ -1,12 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { FooterPage } from './footer.page';
 
 const routes: Routes = [
   {
+    path: 'footer',
+    component: FooterPage,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
+      },
+      {
+        path: 'my-account',
+        loadChildren: () => import('../my-account/my-account.module').then(m => m.MyAccountPageModule)
+      },
+      {
+        path: 'promotions',
+        loadChildren: () => import('../promotions/promotions.module').then(m => m.PromotionsPageModule)
+      },
+      {
+        path: 'promotion',
+        loadChildren: () => import('../location/location.module').then(m => m.LocationPageModule)
+      },
+      {
+        path: '',
+        redirectTo: '/footer/home',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
     path: '',
-    component: FooterPage
+    redirectTo: '/footer/home',
+    pathMatch: 'full'
   }
 ];
 
